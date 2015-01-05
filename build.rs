c9d125ef456c26568bd8de388f595613b2003ff1
Paul Woolcock <paul@woolcock.us>
2015-01-05 13:11:17 -0500

    Must use absolute paths instead of relative paths, if you want it to work under both `cargo build` as well as `cargo publish`

--- build.rs
+++ build.rs
@@ -3,9 +3,10 @@ use std::os;
 use std::io::process::InheritFd;
 
 fn main() {
+    let cwd = Path::new(os::getenv("PWD").unwrap());
     let out_dir = Path::new(os::getenv("OUT_DIR").unwrap());
-    let wren_dir = Path::new("src/wren");
-    let wren_lib = Path::new("src/wren/libwren.a");
+    let wren_dir = cwd.join(Path::new("src/wren"));
+    let wren_lib = cwd.join(Path::new("src/wren/libwren.a"));
 
     let mut make = Command::new("make");
 
@@ -20,5 +21,6 @@ fn main() {
     if let Err(_) = fs::copy(&wren_lib, &out_dir.join("libwren.a")) {
         println!("ERROR COPYING libwren.a");
     }
+
     println!("cargo:rustc-flags=-L {} -l wren:static", out_dir.display());
 }
