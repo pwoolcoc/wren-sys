f69e55808852e1dfb6424753aeca1391652b22b0
Paul Woolcock <paul@woolcock.us>
2016-05-02 14:55:17 -0400

    update to build new wren

--- build.rs
+++ build.rs
@@ -6,18 +6,19 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let manifest_path = Path::new(&manifest_dir);
     let src_dir = manifest_path.join("src");
-    let wren_dir = src_dir.join("wren/lib");
+    let wren_make_dir = src_dir.join("wren");
+    let wren_lib_dir = src_dir.join("wren/lib");
 
     let mut make = Command::new("make");
 
-    assert!(make.current_dir(&Path::new("src/wren"))
-                .arg("release")
+    assert!(make.current_dir(&Path::new(&wren_make_dir))
+                .arg("static")
                 .stdout(Stdio::inherit())
                 .stderr(Stdio::inherit())
                 .status()
                 .unwrap()
                 .success());
 
-    println!("cargo:rustc-link-search=native={}", wren_dir.display());
+    println!("cargo:rustc-link-search=native={}", wren_lib_dir.display());
     println!("cargo:rustc-link-lib=static=wren");
 }
